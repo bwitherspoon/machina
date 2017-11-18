59cf8561ced336000925d1e78c4e4f12f006ff6e
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-17 22:43:35 -0600

    Add logistic activation function module

---
+++ rtl/logistic.sv
@@ -0,0 +1,106 @@
+module logistic (
+  input logic clock,
+  input logic reset,
+  input logic train,
+
+  input logic argument_valid,
+  input logic [15:0] argument_data,
+  output logic argument_ready,
+
+  input logic feedback_valid,
+  input logic [15:0] feedback_data,
+  output logic feedback_ready,
+
+  output logic activation_valid,
+  output logic [7:0] activation_data,
+  input logic activation_ready,
+
+  output logic delta_valid,
+  output logic [15:0] delta_data,
+  input logic delta_ready
+);
+  function real f(int value, real rate = 1.0);
+    return 1.0 / (1.0 + $exp(-rate * $itor(value) / 2.0**8));
+  endfunction
+
+  logic [7:0] activation [2**12];
+  logic [7:0] result;
+  logic [7:0] derivative;
+  logic signed [15:0] feedback;
+  logic signed [15:0] delta;
+
+  enum logic [1:0] { ARG, ACT, FBK, DEL } state = ARG;
+
+  initial begin
+    for (int n = -6 <<< 8; n < 6 <<< 8; n++)
+      activation[n[11:0]] = 8'($rtoi(2.0**8 * f(n)));
+  end
+
+  always @ (posedge clock) begin
+    case (state)
+      ARG:
+        if (argument_valid & argument_ready)
+          state <= ACT;
+      ACT:
+        if (activation_valid & activation_ready)
+          state <= (train) ? FBK : ARG;
+      FBK:
+        if (feedback_valid & feedback_ready)
+          state <= DEL;
+      DEL:
+        if (delta_valid & delta_ready)
+          state <= ARG;
+      default:
+        $error("Invalid state: %h", state);
+    endcase
+  end
+
+  assign argument_ready = state == ARG;
+
+  always @ (posedge clock) begin
+    if (argument_valid & argument_ready) begin
+      if ($signed(argument_data) >= 6 <<< 8)
+        result <= 8'hff;
+      else if ($signed(argument_data) < -6 <<< 8)
+        result <= 8'h00;
+      else
+        result <= activation[argument_data[11:0]];
+    end
+  end
+
+  always @ (posedge clock) begin
+    if (reset) begin
+      activation_valid <= '0;
+    end else if (state == ACT) begin
+      if (!activation_valid) begin
+        activation_valid <= '1;
+        activation_data <= result;
+      end else if (activation_ready) begin
+        activation_valid <= '0;
+      end
+    end
+  end
+
+  assign feedback_ready = state == FBK;
+
+  always @ (posedge clock) begin
+    if (feedback_valid & feedback_ready) begin
+      feedback <= feedback_data;
+      derivative <= result * (2**8 - result) >>> 8;
+    end
+  end
+
+  always @ (posedge clock) begin
+    if (reset) begin
+      delta_valid <= '0;
+    end else if (state == DEL) begin
+      if (!delta_valid) begin
+        delta_valid <= '1;
+        delta_data <= feedback * $signed({1'b0, derivative}) >>> 8;
+      end else if (delta_ready) begin
+        delta_valid <= '1;
+      end
+    end
+  end
+
+endmodule
---
+++ sim/hdl/logistic_test.sv
@@ -0,0 +1,93 @@
+module logistic_test;
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  bit clock = 0;
+  always #5 clock = ~clock;
+
+  bit reset = 0;
+  bit train = 0;
+
+  logic argument_valid = '0;
+  logic argument_ready;
+  logic [15:0] argument_data;
+
+  logic feedback_valid = '0;
+  logic feedback_ready;
+  logic [15:0] feedback_data;
+
+  logic activation_valid;
+  logic activation_ready = '0;
+  logic [7:0] activation_data;
+
+  logic delta_valid;
+  logic delta_ready = '0;
+  logic [15:0] delta_data;
+
+  logic [7:0] activation;
+  logic [15:0] delta;
+
+  logistic dut (
+    .clock(clock),
+    .reset(reset),
+    .train(train),
+    .argument_valid(argument_valid),
+    .argument_data(argument_data),
+    .argument_ready(argument_ready),
+    .feedback_valid(feedback_valid),
+    .feedback_data(feedback_data),
+    .feedback_ready(feedback_ready),
+    .activation_valid(activation_valid),
+    .activation_data(activation_data),
+    .activation_ready(activation_ready),
+    .delta_valid(delta_valid),
+    .delta_data(delta_data),
+    .delta_ready(delta_ready)
+  );
+
+  initial begin
+    $dumpfile("logistic_test.vcd");
+    $dumpvars;
+
+    reset = 1;
+    #20 reset = 0;
+
+    #10 argument_valid = 1;
+    argument_data = 0;
+    wait (argument_ready == 1) @ (posedge clock);
+    #1 argument_valid = 0;
+
+    wait (activation_valid == 1) #1 activation_ready = 1;
+    @ (posedge clock) activation = activation_data;
+    if (activation != 8'h80)
+      $fatal(0, "activation invalid: %h", activation);
+    #1 activation_ready = 0;
+
+    wait (argument_ready == 1) @ (posedge clock) #1 train = 1;
+
+    argument_valid = 1;
+    argument_data = 6 <<< 8;
+    wait (argument_ready == 1) @ (posedge clock);
+    #1 argument_valid = 0;
+
+    wait (activation_valid == 1) #1 activation_ready = 1;
+    @ (posedge clock) activation = activation_data;
+    if (activation != 8'hff)
+      $fatal(0, "activation invalid: %h", activation);
+    #1 activation_ready = 0;
+
+    feedback_valid = 1;
+    feedback_data = -(2**8) - 2**8;
+    wait (feedback_ready == 1) @ (posedge clock);
+    #1 feedback_valid = 0;
+
+    wait (delta_valid == 1) #1 delta_ready = 1;
+    @ (posedge clock) delta = delta_data;
+    if (delta != 8'h00)
+      $fatal(0, "delta invalid: %h", delta);
+    #1 delta_ready = 0;
+
+    $finish(0);
+  end
+
+endmodule
