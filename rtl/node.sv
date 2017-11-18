cf80563af6bbd464bd1e806cad0fa422475ce1a8
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-18 10:06:42 -0600

    Rework node module to use external activation

--- rtl/node.sv
+++ rtl/node.sv
@@ -6,70 +6,68 @@ module node #(
   input logic reset,
   input logic train,
 
-  input  logic input_forward_valid,
-  input  logic [N-1:0][7:0] input_forward_data,
-  output logic input_forward_ready,
+  input  logic operand_valid,
+  input  logic [N-1:0][7:0] operand_data,
+  output logic operand_ready,
 
-  input  logic input_backward_valid,
-  input  logic [15:0] input_backward_data,
-  output logic input_backward_ready,
+  output logic product_valid,
+  output logic [15:0] product_data,
+  input  logic product_ready,
 
-  output logic output_backward_valid,
-  output logic [N-1:0][15:0] output_backward_data,
-  input  logic output_backward_ready,
+  input  logic delta_valid,
+  input  logic [15:0] delta_data,
+  output logic delta_ready,
 
-  output logic output_forward_valid,
-  output logic [7:0] output_forward_data,
-  input  logic output_forward_ready
+  output logic feedback_valid,
+  output logic [N-1:0][15:0] feedback_data,
+  input  logic feedback_ready
 );
-
   localparam W = 8;
-  typedef logic signed [W:0] standard_t;
-  typedef logic signed [2*$bits(standard_t)-1:0] extended_t;
+  typedef logic signed [W:0] std_t;
+  typedef logic signed [2*$bits(std_t)-1:0] ext_t;
 
-  typedef logic [$clog2(N)-1:0] counter_t;
-  localparam CNT = counter_t'(N - 1);
-  counter_t counter;
+  typedef logic [$clog2(N)-1:0] cnt_t;
+  localparam CNT = cnt_t'(N - 1);
+  cnt_t counter;
 
-  standard_t operand [N];
-  extended_t weight [N];
-  extended_t bias;
-  extended_t summand;
-  extended_t accumulator;
-  extended_t delta;
+  std_t operand [N];
+  ext_t weight [N];
+  ext_t bias = '0;
+  ext_t summand = '0;
+  ext_t accumulator = '0;
+  ext_t delta = '0;
 
-  enum logic [2:0] { RDY, MUL, MAC, ACC, FWD, DEL, BWD, UPD } state;
+  logic [15:0] error [N];
 
-  // Initialize logistic activation function and its derivative
-  // TODO We really only need [-6, 6] domain
-  standard_t activation [2**(2*W)];
-  standard_t activation_derivative [2**(2*W)];
+  enum logic [3:0] { RDY, MUL, MAC, ACC, PRD, DEL, ERR, FBK, UPD } state;
 
-  function real logistic(int value, real rate = 6.0);
-    return 1.0 / (1.0 + $exp(-rate * $itor(value) / 2.0**W));
-  endfunction
-
+  // Initialize weights to small pseudorandom values and operands to zero
   initial begin
-    for (int i = -2**(2*W-1); i < 2**(2*W-1); i = i + 1) begin
-      activation[i[2*W-1:0]] = standard_t'($rtoi(2**W * logistic(i)));
-      activation_derivative[i[2*W-1:0]] = standard_t'($rtoi(2**W * logistic(i) * (1 - logistic(i))));
-    end
-  end
-
-  // Initialize weights
-  // TODO Should be initialized uniformly random
-  initial begin
-    bias = 0;
     for (int i = 0; i < N; i = i + 1) begin
-      weight[i] = extended_t'($random % 16);
+      weight[i] = ext_t'($random % 16);
+      operand[i] = '0;
     end
   end
 
-  // Count multiply and accumalate cycles
+  // Load operands
+  assign operand_ready = state == RDY;
+
+  genvar n;
+  generate
+    for (n = 0; n < N; n = n + 1) begin
+      always @(posedge clock) begin
+        if (operand_valid & operand_ready) begin
+          operand[n] <= std_t'(operand_data[n]);
+        end
+      end
+    end
+  endgenerate
+
+  // Cycle counter
   always @(posedge clock) begin
     if (reset) begin
       counter <= '0;
-    end else if (state == MUL || state == MAC) begin
+    end else if (state == MUL || state == MAC || state == ERR || state == UPD) begin
       if (counter == CNT) begin
         counter <= '0;
       end else begin
@@ -78,7 +76,7 @@ module node #(
     end
   end
 
-  // Multiply and accumalate input operands
+  // Multiply and accumulate
   always @(posedge clock) begin
     if (state == MUL || state == MAC) begin
       summand <= (weight[counter] * operand[counter]) >>> W;
@@ -95,78 +93,62 @@ module node #(
     end
   end
 
-  // Load input on interface handshake when in ready state
-  assign input_forward_ready = state == RDY;
-
-  genvar i;
-  generate
-    for (i = 0; i < N; i = i + 1) begin
-      always @(posedge clock) begin
-        if (input_forward_valid & input_forward_ready) begin
-          operand[i] <= standard_t'(input_forward_data[i]);
-        end
-      end
-    end
-  endgenerate
-
-  // Output activation on interface handshake when in forward pass state
-  always @ (posedge clock) begin
-    if (reset) begin
-      output_forward_valid <= '0;
-    end else if (state == FWD) begin
-      if (!output_forward_valid | output_forward_ready) begin
-        output_forward_valid <= '1;
-        output_forward_data <= activation[accumulator[2*W-1:0]][W-1:0];
-      end
-    end else if (output_forward_valid & output_forward_ready) begin
-        output_forward_valid <= '0;
-    end
-  end
-
-  // Calculate delta on interface handshake when in delta state
-  assign input_backward_ready = state == DEL;
-
-  always @ (posedge clock) begin
-    if (input_backward_valid & input_backward_ready) begin
-      delta <= $signed(input_backward_data) * activation_derivative[accumulator[2*W-1:0]] >>> W;
-    end
-  end
-
-  // Backward propagate errors and update weights when in the backward pass state
-  // FIXME Use counter and single multiplier
-  genvar j;
-  generate
-    for (j = 0; j < N; j = j + 1) begin
-      always @(posedge clock) begin
-        if (state == BWD) begin
-          if (!output_backward_valid | output_backward_ready) begin
-            output_backward_data[j] <= 16'((weight[j] * delta) >>> W);
-            weight[j] <= weight[j] + (delta * operand[j] >>> K + W);
-          end
-        end
-      end
-    end
-  endgenerate
-
+  // Output cross product
   always @(posedge clock) begin
     if (reset) begin
-      bias <= '0;
-    end else if (state == BWD) begin
-      if (!output_backward_valid | output_backward_ready) begin
-        bias <= bias + delta >>> K;
+      product_valid <= '0;
+    end else if (state == PRD) begin
+      if (!product_valid) begin
+        product_valid <= '1;
+        product_data <= accumulator[15:0];
+      end else if (product_ready) begin
+        product_valid <= '0;
       end
     end
   end
 
+  // Load delta
+  assign delta_ready = state == DEL;
+
+  always @ (posedge clock) begin
+    if (delta_valid & delta_ready)
+      delta <= ext_t'($signed(delta_data));
+  end
+
+  // Compute error terms
+  always @ (posedge clock) begin
+    if (state == ERR)
+      error[counter] <= 16'(weight[counter] * delta >>> W);
+  end
+
+  // Backward propagate errors
   always @ (posedge clock) begin
     if (reset) begin
-      output_backward_valid <= '0;
-    end else if (state == BWD) begin
-      if (!output_backward_valid | output_backward_ready) begin
-        output_backward_valid <= '1;
+      feedback_valid <= '0;
+    end else if (state == FBK) begin
+      if (!feedback_valid) begin
+        feedback_valid <= '1;
+      end else if (feedback_ready) begin
+        feedback_valid <= '0;
       end
-    end else if (output_backward_valid & output_backward_ready) begin
-        output_backward_valid <= '0;
+    end
+  end
+
+  genvar k;
+  generate
+    for (k = 0; k < N; k = k + 1) begin
+      always @ (posedge clock) begin
+        if (state == FBK && feedback_valid != '1)
+            feedback_data[k] <= error[k];
+      end
+    end
+  endgenerate
+
+  // Update weights
+  // TODO reset
+  always @ (posedge clock) begin
+    if (state == UPD) begin
+      weight[counter] <= weight[counter] + (delta * operand[counter] >>> K + W);
     end
   end
 
@@ -176,37 +158,33 @@ module node #(
       state <= RDY;
     end else begin
       case (state)
-        RDY: begin
-          if (input_forward_valid) begin
+        RDY:
+          if (operand_valid)
             state <= MUL;
-          end
-        end
-        MUL: state <= MAC;
-        MAC: begin
-          if (counter == CNT) begin
+        MUL:
+          state <= MAC;
+        MAC:
+          if (counter == CNT)
             state <= ACC;
-          end
-        end
-        ACC: state <= FWD;
-        FWD: begin
-          if (!output_forward_valid | output_forward_ready) begin
-            if (train)
-              state <= DEL;
-            else
-              state <= RDY;
-          end
-        end
-        DEL: begin
-          if (input_backward_valid) begin
-            state <= BWD;
-          end
-        end
-        BWD: begin
-          if (!output_backward_valid | output_backward_ready) begin
+        ACC:
+          state <= PRD;
+        PRD:
+          if (product_valid & product_ready)
+            state <= (train) ? DEL : RDY;
+        DEL:
+          if (delta_valid & delta_ready)
+            state <= ERR;
+        ERR:
+          if (counter == CNT)
+            state <= FBK;
+        FBK:
+          if (feedback_valid & feedback_ready)
+            state <= UPD;
+        UPD:
+          if (counter == CNT)
             state <= RDY;
-          end
-        end
-        default: $error("Invalid state");
+        default:
+          $fatal(0, "invalid state: %h", state);
       endcase
     end
   end
---
+++ sim/hdl/node_test.sv
@@ -0,0 +1,95 @@
+module node_test;
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  bit clock = 0;
+  always #5 clock = ~clock;
+
+  bit reset = 0;
+  bit train = 0;
+
+  logic operand_valid = 0;
+  logic operand_ready;
+  logic [1:0][7:0] operand_data;
+
+  logic product_valid;
+  logic product_ready = 0;
+  logic [15:0] product_data;
+
+  logic delta_valid = 0;
+  logic delta_ready;
+  logic [15:0] delta_data;
+
+  logic feedback_valid;
+  logic feedback_ready = 0;
+  logic [1:0][15:0] feedback_data;
+
+  logic [15:0] product;
+  logic [1:0][15:0] feedback;
+
+  node #(.N(2), .K(2)) dut (
+    .clock(clock),
+    .reset(reset),
+    .train(train),
+    .operand_valid(operand_valid),
+    .operand_data(operand_data),
+    .operand_ready(operand_ready),
+    .product_valid(product_valid),
+    .product_data(product_data),
+    .product_ready(product_ready),
+    .delta_valid(delta_valid),
+    .delta_data(delta_data),
+    .delta_ready(delta_ready),
+    .feedback_valid(feedback_valid),
+    .feedback_data(feedback_data),
+    .feedback_ready(feedback_ready)
+  );
+
+  initial begin
+    $dumpfile("node_test.vcd");
+    $dumpvars;
+
+    reset = 1;
+    #20 reset = 0;
+
+    @ (negedge clock) operand_valid = 1;
+    operand_data[0] = 8'h00;
+    operand_data[1] = 8'h00;
+    wait (operand_ready == 1) @ (posedge clock);
+    #1 operand_valid = 0;
+
+    wait (product_valid == 1) #1 product_ready = 1;
+    @ (posedge clock) product = product_data;
+    if (product != 8'h00)
+      $fatal(0, "product invalid: %h", product);
+    #1 product_ready = 0;
+
+    wait (operand_ready == 1) @ (posedge clock) #1 train = 1;
+
+    operand_valid = 1;
+    operand_data[0] = 8'h00;
+    operand_data[1] = 8'h00;
+    wait (operand_ready == 1) @ (posedge clock);
+    #1 operand_valid = 0;
+
+    wait (product_valid == 1) #1 product_ready = 1;
+    @ (posedge clock) product = product_data;
+    if (product != 8'h00)
+      $fatal(0, "product invalid: %h", product);
+    #1 product_ready = 0;
+
+    @ (negedge clock) delta_valid = 1;
+    delta_data = 16'h0000;
+    wait (delta_ready == 1) @ (posedge clock);
+    #1 delta_valid = 0;
+
+    wait (feedback_valid == 1) #1 feedback_ready = 1;
+    @ (posedge clock) feedback = feedback_data;
+    if (feedback != 16'h00)
+      $fatal(0, "feedback invalid: %h", feedback);
+    #1 feedback_ready = 0;
+
+    $finish(0);
+  end
+
+endmodule
