ff78f96ab7f7fab8c1465625dfb53d798684d323
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-19 17:22:25 -0600

    Refactor node test

--- sim/hdl/node_test.sv
+++ sim/hdl/node_test.sv
@@ -1,4 +1,10 @@
 module node_test;
+`include "test.svh"
+
+  function logic [15:0] abs(logic [15:0] val);
+    return (val[15]) ? -val : val;
+  endfunction
+
   bit clock = 0;
   always #5 clock = ~clock;
 
@@ -21,11 +27,11 @@ module node_test;
   logic propagate_ready = 0;
   logic [1:0][15:0] propagate_data;
 
-  logic [1:0][7:0] argument [4];
-  logic [15:0] target [4];
-  logic [15:0] result;
-  logic signed [15:0] delta;
-  logic [1:0][15:0] propagate;
+  logic [1:0][7:0] arg [4];
+  logic [15:0] tgt [4];
+  logic [15:0] res;
+  logic signed [15:0] err;
+  logic [1:0][15:0] prp;
 
   node #(.N(2), .S(2), .SEED(0)) dut (
     .clock(clock),
@@ -62,9 +68,9 @@ module node_test;
     #1 argument_valid = 0;
 
     wait (result_valid == 1) #1 result_ready = 1;
-    @ (posedge clock) result = result_data;
-    if (result != 16'hfff9) begin
-      $error("result invalid: %h", result);
+    @ (posedge clock) res = result_data;
+    if (res != 16'hfff9) begin
+      $display("ERROR: result invalid: %h", res);
       $stop;
     end
     #1 result_ready = 0;
@@ -78,9 +84,9 @@ module node_test;
     #1 argument_valid = 0;
 
     wait (result_valid == 1) #1 result_ready = 1;
-    @ (posedge clock) result = result_data;
-    if (result != 16'hfff4) begin
-      $error("result invalid: %h", result);
+    @ (posedge clock) res = result_data;
+    if (res != 16'hfff4) begin
+      $display("ERROR: result invalid: %h", res);
       $stop;
     end
     #1 result_ready = 0;
@@ -91,74 +97,75 @@ module node_test;
     #1 error_valid = 0;
 
     wait (propagate_valid == 1) #1 propagate_ready = 1;
-    @ (posedge clock) propagate = propagate_data;
-    if (propagate != 16'h00) begin
-      $error("propagate invalid: %h", propagate);
+    @ (posedge clock) prp = propagate_data;
+    if (prp != 16'h00) begin
+      $display("ERROR: propagation invalid: %h", prp);
       $stop;
     end
     #1 propagate_ready = 0;
 
     // Test 2
-    argument[0] = 16'h0000;
-    argument[1] = 16'h00ff;
-    argument[2] = 16'hff00;
-    argument[3] = 16'hffff;
-    target[0] = 16'hff00;
-    target[1] = 16'h007f;
-    target[2] = 16'hff00;
-    target[3] = 16'h007f;
-
+    arg[0] = 16'h0000;
+    arg[1] = 16'h00ff;
+    arg[2] = 16'hff00;
+    arg[3] = 16'hffff;
+    tgt[0] = 16'hff00;
+    tgt[1] = 16'h007f;
+    tgt[2] = 16'hff00;
+    tgt[3] = 16'h007f;
+    // Train
     repeat (25) begin
       for (int i = 0; i < 4; i++) begin
         argument_valid = 1;
-        argument_data = argument[i];
+        argument_data = arg[i];
         wait (argument_ready == 1) @ (posedge clock);
         #1 argument_valid = 0;
 
         wait (result_valid == 1) #1 result_ready = 1;
-        @ (posedge clock) result = result_data;
+        @ (posedge clock) res = result_data;
         #1 result_ready = 0;
 
         @ (negedge clock) error_valid = 1;
-        error_data = $unsigned($signed(target[i]) - $signed(result));
+        error_data = $unsigned($signed(tgt[i]) - $signed(res));
         wait (error_ready == 1) @ (posedge clock);
         #1 error_valid = 0;
 
         wait (propagate_valid == 1) #1 propagate_ready = 1;
-        @ (posedge clock) propagate = propagate_data;
+        @ (posedge clock) prp = propagate_data;
         #1 propagate_ready = 0;
       end
     end
-
+    // Validate
     for (int i = 0; i < 4; i++) begin
       argument_valid = 1;
-      argument_data = argument[i];
+      argument_data = arg[i];
       wait (argument_ready == 1) @ (posedge clock);
       #1 argument_valid = 0;
 
       wait (result_valid == 1) #1 result_ready = 1;
-      @ (posedge clock) result = result_data;
+      @ (posedge clock) res = result_data;
       #1 result_ready = 0;
 
       @ (negedge clock) error_valid = 1;
-      delta = $signed(target[i]) - $signed(result);
-      error_data = $unsigned(delta);
+      err = $signed(tgt[i]) - $signed(res);
+      error_data = $unsigned(err);
 `ifdef DEBUG
       $write("DEBUG: ");
-      $write("%6.3f * %6.3f + ", dut.weight[1] / 256.0, argument[i][1] / 256.0);
-      $write("%6.3f * %6.3f + ", dut.weight[0] / 256.0, argument[i][0] / 256.0);
-      $write("%6.3f = %6.3f ? ", dut.bias / 256.0, $signed(result) / 256.0);
-      $write("%6.3f ! %6.3f\n", $signed(target[i]) / 256.0, $signed(delta) / 256.0);
+      $write("%6.3f * %6.3f + ", dut.weight[1] / 256.0, arg[i][1] / 256.0);
+      $write("%6.3f * %6.3f + ", dut.weight[0] / 256.0, arg[i][0] / 256.0);
+      $write("%6.3f = %6.3f ? ", dut.bias / 256.0, $signed(res) / 256.0);
+      $write("%6.3f ! %6.3f\n", $signed(tgt[i]) / 256.0, $signed(err) / 256.0);
 `endif
       wait (error_ready == 1) @ (posedge clock);
       #1 error_valid = 0;
-      if (((delta[15]) ? -delta : delta) > 4) begin
-        $display("ERROR: error out of range: %6.3f", delta / 256.0);
+      if (abs(err) > 4) begin
+        $display("ERROR: error out of range: %6.3f", err / 256.0);
         $stop;
       end
 
+      // TODO verify propagation
       wait (propagate_valid == 1) #1 propagate_ready = 1;
-      @ (posedge clock) propagate = propagate_data;
+      @ (posedge clock) prp = propagate_data;
       #1 propagate_ready = 0;
     end
     // Success
