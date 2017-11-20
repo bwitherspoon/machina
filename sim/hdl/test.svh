61157243d4552f2ace9b3bbd14e1157392c3dc9d
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-20 13:17:34 -0600

    Eliminate test helper tasks except forward and backward

--- sim/hdl/test.svh
+++ sim/hdl/test.svh
@@ -29,50 +29,17 @@ task test;
   end
 endtask
 
-task argument;
-  input [`TEST_WIDTH-1:0] data;
-  begin
-    argument_valid = 1;
-    argument_data = data;
-    wait (argument_ready) @ (posedge clock);
-    #1 argument_valid = 0;
-  end
-endtask
-
-task result;
-  output [`TEST_WIDTH-1:0] data;
-  begin
-    wait (result_valid) #1 result_ready = 1;
-    @ (posedge clock) data = result_data;
-    #1 result_ready = 0;
-  end
-endtask
-
-task error;
-  input [`TEST_WIDTH-1:0] data;
-  begin
-    error_valid = 1;
-    error_data = data;
-    wait (error_ready) @ (posedge clock);
-    #1 error_valid = 0;
-  end
-endtask
-
-task propagate;
-  output [`TEST_WIDTH-1:0] data;
-  begin
-    wait (propagate_valid) #1 propagate_ready = 1;
-    @ (posedge clock) data = propagate_data;
-    #1 propagate_ready = 0;
-  end
-endtask
-
 task forward;
   input [`TEST_WIDTH-1:0] arg;
   output [`TEST_WIDTH-1:0] res;
   begin
-    argument(arg);
-    result(res);
+    argument_valid = 1;
+    argument_data = arg;
+    wait (argument_ready) @ (posedge clock);
+    #1 argument_valid = 0;
+    wait (result_valid) #1 result_ready = 1;
+    @ (posedge clock) res = result_data;
+    #1 result_ready = 0;
   end
 endtask
 
@@ -80,8 +47,13 @@ task backward;
   input [`TEST_WIDTH-1:0] err;
   output [`TEST_WIDTH-1:0] prp;
   begin
-    error(err);
-    propagate(prp);
+    error_valid = 1;
+    error_data = err;
+    wait (error_ready) @ (posedge clock);
+    #1 error_valid = 0;
+    wait (propagate_valid) #1 propagate_ready = 1;
+    @ (posedge clock) prp = propagate_data;
+    #1 propagate_ready = 0;
   end
 endtask
 
