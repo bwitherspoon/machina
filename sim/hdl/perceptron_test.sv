00b79875c3617557af0cda6b4c635fdc7114f62a
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-20 16:02:45 -0600

    Use test macro and cleanup

--- sim/hdl/perceptron_test.sv
+++ sim/hdl/perceptron_test.sv
@@ -31,50 +31,39 @@ module perceptron_test;
   logic signed [15:0] err;
   logic [1:0][15:0] prp;
 
-  int count;
-
   initial begin
 `ifdef DUMPFILE
     $dumpfile(`"`DUMPFILE`");
     $dumpvars;
 `endif
-    arg[0] = 16'h0000; arg[1] = 16'h00ff; arg[2] = 16'hff00; arg[3] = 16'hffff;
     // Test 1 (AND)
+    arg[0] = 16'h0000; arg[1] = 16'h00ff; arg[2] = 16'hff00; arg[3] = 16'hffff;
     tgt[0] = 8'h00; tgt[1] = 8'h00; tgt[2] = 8'h00; tgt[3] = 8'hff;
     err = 16'h7FFF;
     reset = 1;
     repeat (2) @ (posedge clock);
     #1 reset = 0;
     train = 1;
-    count = 0;
-    while (abs(err) > 5 && count < 1000) begin
+    repeat (10) begin
       for (int i = 0; i < 4; i++) begin
         forward(arg[i], res);
         err = $signed({1'b0, tgt[i]}) - $signed({1'b0, res});
         backward(err, prp);
-`ifdef DEBUG
-        $write("DEBUG: %3d: ", count);
-        $write("%6.3f * %6.3f + ", neuron.associate.weight[1]/256.0, arg[i][1]/256.0);
-        $write("%6.3f * %6.3f + ", neuron.associate.weight[0]/256.0, arg[i][0]/256.0);
-        $write("%6.3f = %6.3f ? ", neuron.associate.bias/256.0, res/256.0);
-        $write("%6.3f ! %6.3f\n", tgt[i]/256.0, err/256.0);
-`endif
       end
-      count++;
     end
-//     train = 0;
-//     for (int i = 0; i < 4; i++) begin
-//       forward(arg[i], res);
-//       err = $signed({1'b0, tgt[i]}) - $signed({1'b0, res});
-// `ifdef DEBUG
-//       $write("DEBUG: %5d: ", count);
-//       $write("%6.3f * %6.3f + ", neuron.associate.weight[1]/256.0, arg[i][1]/256.0);
-//       $write("%6.3f * %6.3f + ", neuron.associate.weight[0]/256.0, arg[i][0]/256.0);
-//       $write("%6.3f = %6.3f ? ", neuron.associate.bias/256.0, res/256.0);
-//       $write("%6.3f ! %6.3f\n", tgt[i]/256.0, err/256.0);
-// `endif
-//       test(abs(err) <= 5);
-//     end
+    train = 0;
+    for (int i = 0; i < 4; i++) begin
+      forward(arg[i], res);
+      err = $signed({1'b0, tgt[i]}) - $signed({1'b0, res});
+`ifdef DEBUG
+      $write("DEBUG: ");
+      $write("%4.1f * %1.0f + ", neuron.associate.weight[1]/256.0, arg[i][1]/256.0);
+      $write("%4.1f * %1.0f + ", neuron.associate.weight[0]/256.0, arg[i][0]/256.0);
+      $write("%4.1f = %1.0f ? ", neuron.associate.bias/256.0, res/256.0);
+      $write("%1.0f ! %2.0f\n", tgt[i]/256.0, err/256.0);
+`endif
+      `TEST(abs(err) == 0);
+    end
     // Success
     $finish;
   end
