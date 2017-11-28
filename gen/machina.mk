0b084df8057dc40730ecaf2d44eb73cf22aea218
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-28 01:27:03 -0600

    gen: more make fixes

--- gen/machina.mk
+++ gen/machina.mk
@@ -6,24 +6,28 @@ DATDIR := $(GENDIR)dat/
 vpath %.cc $(SRCDIR)
 vpath %.h $(SRCDIR)
 
-gen-all: $(BINDIR)mem $(DATDIR)sigmoid.dat $(DATDIR)sigmoid_prime.dat
+gen-all: gen-bin gen-dat
+
+gen-bin: $(BINDIR)mem
+
+gen-dat: $(DATDIR)sigmoid.dat $(DATDIR)sigmoid_prime.dat
+
+$(BINDIR) $(DATDIR):
+	@mkdir -p $@
 
 $(BINDIR)mem: LDFLAGS += -lboost_program_options
 $(BINDIR)mem: driver.cc memory.h sigmoid.h | $(BINDIR)
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $<
 
-$(BINDIR) $(DATDIR):
-	@mkdir -p $@
+$(DATDIR)sigmoid.dat: $(BINDIR)mem | $(DATDIR)
+	$(PRJDIR)$< -f sigmod -w 8 -d 4096 > $@
 
-$(DATDIR)%_prime.dat: $(BINDIR)mem | $(DATDIR)
-	$(PRJDIR)$< -p -f $(notdir $*) > $@
-
-$(DATDIR)%.dat: $(BINDIR)mem | $(DATDIR)
-	$(PRJDIR)$< -f $(notdir $*) > $@
-
-clean: gen-clean
+$(DATDIR)sigmoid_prime.dat: $(BINDIR)mem | $(DATDIR)
+	$(PRJDIR)$< -f sigmod -p -w 6 -d 4096 > $@
 
 gen-clean:
 	-$(RM) -r $(BINDIR) $(DATDIR)
 
-.PHONY: clean gen-all gen-clean
+clean: gen-clean
+
+.PHONY: gen-all gen-bin gen-dat gen-clean
