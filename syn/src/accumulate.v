c9e5bd861ed5e95758d212b5ad083e8e4c0dc8ad
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-29 01:53:07 -0600

    syn: accumulate: add more to unfinished module to pass lint

--- syn/src/accumulate.v
+++ syn/src/accumulate.v
@@ -1,32 +1,43 @@
 module accumulate #(
-  parameter N = 2,
-  parameter W = 16
+  parameter WIDTH = 16,
+  parameter DEPTH = 2
 )(
   input clk,
   input rst,
 
-  input [N-1:0] arg_stb,
-  input [N*W-1:0] arg_dat,
-  output reg [N-1:0] arg_rdy,
+  input [DEPTH-1:0] arg_stb,
+  input [DEPTH*WIDTH-1:0] arg_dat,
+  output reg [DEPTH-1:0] arg_rdy,
 
-  output res_stb,
-  output [W-1:0] res_dat,
+  output reg res_stb,
+  output reg [DEPTH+WIDTH-1:0] res_dat,
   input res_rdy
 );
-  localparam RES_MAX = 2**(W-1)-1;
-  localparam RES_MIN = -(2**(W-1));
-
-  reg [W:0] acc = 0;
+  reg [DEPTH+WIDTH-1:0] acc = 0;
 
   integer n;
   always @ (*) begin
     arg_rdy = 0;
-    for (n = N - 1; n >= 0; n = n - 1) begin
+    for (n = DEPTH - 1; n >= 0; n = n - 1) begin
       if (arg_stb[n]) begin
         arg_rdy = 0;
         arg_rdy[n] = 1;
       end
     end
   end
+
+  initial res_stb = 0;
+  always @(posedge clk) begin
+    if (rst) begin
+      res_stb <= 0;
+    end else if (!res_stb) begin
+      res_stb <= 1;
+      res_dat <= acc;
+    end else if (res_rdy) begin
+      res_stb <= 0;
+    end
+  end
+
+  wire [DEPTH*WIDTH-1:0] unused =  arg_dat;
 
  endmodule
