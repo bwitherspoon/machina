8ed40855a371aae0b2c84e4137b31ec2e159ac52
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-30 19:15:15 -0600

    syn: make; add sigmoid prereqs and filter sources

--- syn/machina.mk
+++ syn/machina.mk
@@ -29,13 +29,15 @@ $(SYN_CHECK):: check-%: %.v
 	@$(VERILATOR) $(VERILATOR_FLAGS) --lint-only $<
 	@$(YOSYS) -q $<
 
+$(SYN_BLIF_DIR)sigmoid.blif:: memory.v $(GEN_SIG_ACT) $(GEN_SIG_ACT)
+
 $(SYN_BLIF_DIR)%.blif:: %.v | $(SYN_BLIF_DIR) $(SYN_LOG_DIR)
 	@if [ -e '$(SYN_DIR)$*.ys' ]; then \
 		echo '$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -s $(SYN_DIR)$*.ys'; \
 		$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -s $(SYN_DIR)$*.ys; \
 	else \
-		echo '$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -S $<'; \
-		$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -S $<; \
+		echo '$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -S $(filter %.v,$^)'; \
+		$(YOSYS) -q -l $(SYN_LOG_DIR)$*-blif.log -o $@ -S $(filter %.v,$^); \
 	fi
 
 $(SYN_BLIF_DIR) $(SYN_LOG_DIR):
