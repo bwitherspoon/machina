b8269a86e156642e702c34360f624d4e1d558c6c
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-09 14:48:14 -0600

    syn: receive: fix logic error in receiver

--- syn/src/receive.v
+++ syn/src/receive.v
@@ -9,22 +9,21 @@ module receive #(
   output reg stb,
   output reg [7:0] dat
 );
-  localparam integer CYCLES = FREQ / BAUD;
-  localparam COUNT_WIDTH = $clog2(3*CYCLES/2);
-  localparam COUNT = CYCLES[COUNT_WIDTH-1:0];
+  localparam integer PERIOD = FREQ / BAUD;
+  localparam COUNT = PERIOD[$clog2(3*PERIOD/2)-1:0];
+  localparam [3:0] IDLE = 0;
+  localparam [3:0] START = 1;
+  localparam [3:0] STOP = 10;
 
-  localparam START = 0;
-  localparam VALID = 10;
-
-  reg [COUNT_WIDTH-1:0] count = 0;
-  reg [3:0] state = START;
+  reg [$clog2(3*PERIOD/2)-1:0] count = 0;
+  reg [3:0] state = IDLE;
   reg [7:0] data;
 
   initial stb = 0;
   always @(posedge clk) begin
     if (rst) begin
       stb <= 0;
-    end else if (state == VALID) begin
+    end else if (state == STOP) begin
       if (~stb) begin
         stb <= 1;
         dat <= data;
@@ -38,28 +37,28 @@ module receive #(
 
   always @(posedge clk) begin
     if (rst) begin
+      state <= IDLE;
       count <= 0;
-      state <= START;
-    end else if (state == START) begin
-      if (!rxd) begin
-        count <= COUNT >> 1;
-        state <= 1;
-      end
     end else begin
-      if (count != COUNT) begin
-        count <= count + 1;
-      end else begin
-        if (state == VALID) begin
-          if (~stb | rdy) begin
-            count <= 0;
-            state <= 0;
-          end
-        end else begin
-          count <= 0;
-          state <= state + 1;
-          data <= {rxd, data[7:1]};
+      case (state)
+      IDLE:
+        if (~rxd) begin
+          state <= START;
+          count <= COUNT >> 1;
         end
-      end
+      STOP:
+        if (~stb | rdy) begin
+          state <= IDLE;
+        end
+      default:
+        if (count == COUNT) begin
+          data <= {rxd, data[7:1]};
+          state <= state + 1;
+          count <= 0;
+        end else begin
+          count <= count + 1;
+        end
+      endcase
     end
   end
 
