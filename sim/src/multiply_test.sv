63af48680db1ebf72ae930b48a14096df0a7f826
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-11 23:44:30 -0600

    sim: multiply: update test

--- sim/src/multiply_test.sv
+++ sim/src/multiply_test.sv
@@ -1,29 +1,57 @@
-module top;
-  `define ARGW 16
-  `define ARGD 2
-  `define RESW 32
-  `include "test.svh"
+`include "debug.vh"
+`include "interface.svh"
+`include "random.svh"
 
-  multiply uut (.*);
+module testbench #(
+  parameter ARGW = 16
+);
+  `include "clock.svh"
+  `include "dump.svh"
+  `include "reset.svh"
 
-  logic [RESW-1:0] res;
+  localparam RESW = 2*ARGW;
+
+  `xmt(arg_, ARGW, 2)
+  `rcv(res_, RESW)
+
+  multiply #(ARGW) uut (.*);
+
+  task testcase;
+    logic signed [1:0][ARGW-1:0] arg;
+    logic signed [1:0][RESW-1:0] res;
+    repeat (8) begin
+      arg[0] = random(2**16-1);
+      arg[1] = random(2**16-1);
+      res[0] = arg[0] * arg[1];
+      arg_xmt(arg);
+      res_rcv(res[1]);
+      `ASSERT_EQUAL(res[0], res[1]);
+    end
+  endtask
 
   task test;
-    begin
-      forward(32'h0080_0080, res);
-      `ASSERT_EQUAL(res, 32'h4000);
-      forward(32'h0100_0100, res);
-      `ASSERT_EQUAL(res, 32'h10000);
-      forward(32'h7fff_0000, res);
-      `ASSERT_EQUAL(res, 32'b0);
-    end
+    fork
+      begin : timeout
+        repeat (1e6) @(posedge clk);
+        disable worker;
+        $error("testbench timeout");
+        $stop;
+      end : timeout
+      begin : worker
+        testcase;
+        disable timeout;
+      end : worker
+    join
   endtask
 
   initial begin
     dump;
+    seed;
+    #PERIOD;
     test;
     reset;
     test;
     $finish;
   end
+
 endmodule
