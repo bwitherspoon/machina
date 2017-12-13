f08f1fdbd2944af0f61de7bdbe21298b4cac80b4
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-13 12:33:46 -0600

    sim: fix saturate test

--- sim/src/saturate_test.sv
+++ sim/src/saturate_test.sv
@@ -1,26 +1,64 @@
+`include "clock.svh"
+`include "dump.svh"
+`include "interface.svh"
+`include "reset.svh"
+`include "test.svh"
+
 module testbench;
-  // `define ARGW 24
-  // `define RESW 16
-  // `include "test.svh"
-  //
-  // saturate #(.ARGW(ARGW), .RESW(RESW)) uut (.*);
-  //
-  // logic [RESW-1:0] res;
-  //
-  // task test0;
-  // begin
-  //   forward(24'h000ff, res);
-  //   `ASSERT_EQUAL(res, 16'h00ff);
-  //   forward(24'hffff00, res);
-  //   `ASSERT_EQUAL(res, 16'hff00);
-  //   forward(24'h7fffff, res);
-  //   `ASSERT_EQUAL(res, 16'h7fff);
-  //   forward(24'h800000, res);
-  //   `ASSERT_EQUAL(res, 16'h8000);
-  //   end
-  // endtask
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  localparam ARGW = 24;
+  localparam RESW = 16;
+
+  `clock()
+  `reset
+  `master(arg_, ARGW)
+  `slave(res_, RESW)
+
+  saturate #(.ARGW(ARGW), .RESW(RESW)) uut (.*);
+
+  task testcase;
+    logic [ARGW-1:0] arg [4];
+    logic [RESW-1:0] exp [4];
+    logic [RESW-1:0] res;
+    begin
+      arg[0] = 24'h0000ff; arg[1] = 24'hffff00; arg[2] = 24'h7fffff; arg[3] = 24'h800000;
+      exp[0] = 16'h00ff; exp[1] = 16'hff00; exp[2] = 16'h7fff; exp[3] = 16'h8000;
+      for (int i = 0; i < 4; i++) begin
+        arg_xmt(arg[i]);
+        res_rcv(res);
+        `test_equal(res, exp[i]);
+      end
+    end
+  endtask
+
+  task test;
+  fork
+    begin : timeout
+      repeat (1e6) @(posedge clk);
+      disable worker;
+      `ifdef __ICARUS__
+        $error("testbench timeout");
+        $stop;
+      `else
+        $fatal(0, "testbench timeout");
+      `endif
+    end : timeout
+    begin : worker
+      testcase;
+      disable timeout;
+    end : worker
+  join
+  endtask : test
 
   initial begin
+    dump;
+    #PERIOD;
+    test;
+    reset;
+    test;
     $finish;
   end
+
 endmodule
