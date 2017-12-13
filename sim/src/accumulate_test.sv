8335472ca7afe6d85e013f15e4f07811d821f014
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-12 19:19:53 -0600

    sim: fix tests

--- sim/src/accumulate_test.sv
+++ sim/src/accumulate_test.sv
@@ -1,49 +1,71 @@
+`include "clock.svh"
+`include "dump.svh"
+`include "interface.svh"
+`include "reset.svh"
+`include "test.svh"
+
 module testbench;
-  // `define ARGW 24
-  // `define RESW 40
-  // `include "test.svh"
-  //
-  // accumulate #(.ARGW(ARGW), .RESW(RESW)) uut (.*);
-  //
-  // task test;
-  //   begin
-  //     fork
-  //       begin
-  //         logic [ARGW-1:0] arg [4];
-  //         bit timeout;
-  //         int i;
-  //         arg[0] = 24'h0000ff; arg[1] = 24'h000001;
-  //         arg[2] = 24'hffffff; arg[3] = 24'h00000f;
-  //         for (i = 0; i < 3; i++) begin
-  //           timeout = 0;
-  //           argument(arg[i], timeout);
-  //           `ASSERT_EQUAL(timeout, 0);
-  //         end
-  //         timeout = 0;
-  //         repeat (2) @(posedge clk);
-  //         @(negedge clk) argument(arg[3], timeout);
-  //         `ASSERT_EQUAL(timeout, 0);
-  //       end
-  //       begin
-  //         logic [RESW-1:0] res;
-  //         bit timeout = 0;
-  //         result(res, timeout);
-  //         `ASSERT_EQUAL(timeout, 0);
-  //         `ASSERT_EQUAL(res, 40'h00000000ff);
-  //         timeout = 0;
-  //         result(res, timeout);
-  //         `ASSERT_EQUAL(timeout, 0);
-  //         `ASSERT_EQUAL(res, 40'h000000000f);
-  //       end
-  //     join
-  //   end
-  // endtask
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  localparam ARGW = 24;
+  localparam RESW = 40;
+
+  `clock()
+  `reset
+  `master(arg_, ARGW)
+  `slave(res_, RESW)
+
+  accumulate #(.ARGW(ARGW), .RESW(RESW)) uut (.*);
+
+  task testcase;
+    fork
+      begin : xmt
+        logic [ARGW-1:0] arg [4];
+        bit timeout;
+        int i;
+        arg[0] = 24'h0000ff; arg[1] = 24'h000001;
+        arg[2] = 24'hffffff; arg[3] = 24'h00000f;
+        for (i = 0; i < 3; i++) arg_xmt(arg[i]);
+        repeat (2) @(posedge clk);
+        @(negedge clk) arg_xmt(arg[3]);
+      end : xmt
+      begin : rcv
+        logic [RESW-1:0] res;
+        res_rcv(res);
+        `test_equal(res, 40'h00000000ff);
+        res_rcv(res);
+        `test_equal(res, 40'h000000000f);
+      end : rcv
+    join
+  endtask : testcase
+
+  task test;
+    fork
+      begin : timeout
+        repeat (1e6) @(posedge clk);
+        disable worker;
+        `ifdef __ICARUS__
+          $error("testbench timeout");
+          $stop;
+        `else
+          $fatal(0, "testbench timeout");
+        `endif
+      end : timeout
+      begin : worker
+        testcase;
+        disable timeout;
+      end : worker
+    join
+  endtask : test
 
   initial begin
-    // dump;
-    // test;
-    // reset;
-    // test;
+    dump;
+    #PERIOD;
+    test;
+    reset;
+    test;
     $finish;
   end
+
 endmodule
--- sim/src/associate_test.sv
+++ sim/src/associate_test.sv
@@ -1,57 +1,83 @@
+`include "clock.svh"
+`include "dump.svh"
+`include "interface.svh"
+`include "random.svh"
+`include "reset.svh"
+`include "test.svh"
+`include "util.svh"
+
 module testbench;
-  // `define ARGW 8
-  // `define ARGD 2
-  // `define RESW 16
-  // `define ERRD 1
-  // `include "test.svh"
-  //
-  // logic [ARGD-1:0][ARGW-1:0] arg [4];
-  // logic [RESW-1:0] res;
-  // logic signed [RESW-1:0] tgt [4];
-  // logic signed [RESW-1:0] act;
-  // logic signed [ERRW-1:0] err;
-  // logic [FBKD-1:0][FBKW-1:0] fbk;
-  //
-  // associate #(.ARGD(ARGD), .RATE(1)) uut (.*);
-  //
-  // task train;
-  // begin
-  //   en = 1;
-  //   repeat (25) begin
-  //     for (int i = 0; i < 4; i++) begin
-  //       forward(arg[i], res);
-  //       act = ($signed(res) < 0) ? 16'h0000 : 16'h00ff;
-  //       err = tgt[i] - act;
-  //       backward(err, fbk);
-  //     end
-  //   end
-  //   en = 0;
-  //   for (int i = 0; i < 4; i++) begin
-  //     forward(arg[i], res);
-  //     act = ($signed(res) < 0) ? 16'h0000 : 16'h00ff;
-  //     err = tgt[i] - act;
-  //     `ASSERT_EQUAL(abs(err), 0);
-  //   end
-  // end
-  // endtask : train
-  //
-  // task fwd_test;
-  // begin
-  //   forward(16'h0000, res);
-  //   `ASSERT_EQUAL(res, 16'b0);
-  // end
-  // endtask : fwd_test
-  //
-  // task bwd_test;
-  // begin
-  //   en = 1;
-  //   forward(16'h0000, res);
-  //   `ASSERT_EQUAL(res, 16'b0);
-  //   backward(16'h0000, fbk);
-  //   `ASSERT_EQUAL(fbk, 32'b0);
-  // end
-  // endtask : bwd_test
-  //
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  localparam ARGW = 8;
+  localparam ARGD = 2;
+  localparam RESW = 16;
+  localparam ERRW = 16;
+  localparam FBKW = 16;
+  localparam FBKD = 2;
+
+  `clock()
+  `reset
+  `master(arg_, ARGW, ARGD)
+  `slave(res_, RESW)
+  `master(err_, ERRW)
+  `slave(fbk_, FBKW, FBKD)
+
+  logic en = 0;
+  logic [ARGD-1:0][ARGW-1:0] arg [4];
+  logic [RESW-1:0] res;
+  logic signed [RESW-1:0] tgt [4];
+  logic signed [RESW-1:0] act;
+  logic signed [ERRW-1:0] err;
+  logic [FBKD-1:0][FBKW-1:0] fbk;
+
+  associate #(.ARGD(ARGD), .RATE(1)) uut (.*);
+
+  task train;
+  begin
+    en = 1;
+    repeat (25) for (int i = 0; i < 4; i++) begin
+        arg_xmt(arg[i]);
+        res_rcv(res);
+        act = $signed(res) < 0 ? 16'h0000 : 16'h00ff;
+        err = tgt[i] - act;
+        err_xmt(err);
+        fbk_rcv(fbk);
+    end
+    en = 0;
+    for (int i = 0; i < 4; i++) begin
+      arg_xmt(arg[i]);
+      res_rcv(res);
+      act = $signed(res < 0) ? 16'h0000 : 16'h00ff;
+      err = tgt[i] - act;
+      `test_equal(abs(err), 0);
+    end
+  end
+  endtask : train
+
+  task fwd_test;
+  begin
+    en = 0;
+    arg_xmt(16'h0000);
+    res_rcv(res);
+    `test_equal(res, 16'b0);
+  end
+  endtask : fwd_test
+
+  task bwd_test;
+  begin
+    en = 1;
+    err_xmt(16'h0000);
+    fbk_rcv(res);
+    `test_equal(res, 16'b0);
+    err_xmt(16'h0000);
+    fbk_rcv(res);
+    `test_equal(res, 32'b0);
+    en = 0;
+  end
+  endtask : bwd_test
+
   // task and_test;
   // begin
   // // Logical AND function with linear threshold
@@ -80,18 +106,45 @@ module testbench;
   //   or_test;
   // end
   // endtask : test
-  //
-  // task init;
-  // begin
-  //   for (int i = 0; i < ARGD; i++)
-  //     uut.weights[i] = random(2**4);
-  // end
-  // endtask : init
+
+  task test;
+  fork
+    begin : timeout
+      repeat (1e6) @(posedge clk);
+      disable worker;
+      `ifdef __ICARUS__
+        $error("testbench timeout");
+        $stop;
+      `else
+        $fatal(0, "testbench timeout");
+      `endif
+    end : timeout
+    begin : worker
+      fwd_test;
+      bwd_test;
+      // reset;
+      // and_test;
+      // reset;
+      // or_test;
+      disable timeout;
+    end : worker
+  join
+  endtask : test
+
+  task init;
+  begin
+    for (int i = 0; i < ARGD; i++)
+      uut.weights[i] = random(2**4);
+  end
+  endtask : init
 
   initial begin
-    // dump;
-    // init;
-    // test;
+    dump;
+    seed;
+    init;
+    test;
+    reset;
+    test;
     $finish;
   end
 
