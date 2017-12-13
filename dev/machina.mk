61e2a2d1607e2b15964c43d7403de09cebcc8ddf
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-13 14:22:20 -0600

    dev: rename driver -> memory

--- dev/machina.mk
+++ dev/machina.mk
@@ -20,7 +20,7 @@ clean-dev:
 	-$(RM) -r $(dev_mem)
 
 $(dev_mem): LDFLAGS += -lboost_program_options
-$(dev_mem): driver.cc memory.hh sigmoid.hh -lboost_program_options
+$(dev_mem): memory.cc memory.hh sigmoid.hh -lboost_program_options
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $<
 
 .PHONY: all clean all-dev clean-dev
