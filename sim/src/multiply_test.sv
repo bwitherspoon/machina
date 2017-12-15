c97c605d74702f8112e39fa6d4edb48e318e519f
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-14 21:53:16 -0600

    multiply: use two interfaces for operands

--- sim/src/multiply_test.sv
+++ sim/src/multiply_test.sv
@@ -1,35 +1,66 @@
 `include "clock.svh"
-`include "debug.vh"
 `include "dump.svh"
 `include "interface.svh"
 `include "random.svh"
 `include "reset.svh"
+`include "test.svh"
 
 module testbench;
   timeunit 1ns;
   timeprecision 1ps;
 
   localparam ARGW = 16;
-  localparam ARGD = 2;
+  localparam ARGC = 2;
   localparam RESW = 2*ARGW;
 
   `clock()
   `reset
-  `master(arg_, ARGW, ARGD)
+  `master(arg_, ARGW,, ARGC)
   `slave(res_, RESW)
 
   multiply #(ARGW) uut (.*);
 
-  task testcase;
+  task testcase0;
     logic [1:0][ARGW-1:0] arg;
     logic [1:0][RESW-1:0] res;
     repeat (8) begin
       arg[0] = random(2**16-1);
       arg[1] = random(2**16-1);
       res[0] = $signed(arg[0]) * $signed(arg[1]);
-      arg_xmt(arg);
+      for (int i = 0; i < 2; i++) arg_xmt(arg[i], i);
       res_rcv(res[1]);
-      `ASSERT_EQUAL(res[0], res[1]);
+      `test_equal(res[0], res[1]);
+    end
+  endtask
+
+  task testcase1;
+    logic [1:0][ARGW-1:0] arg;
+    logic [1:0][RESW-1:0] res;
+    repeat (8) begin
+      arg[0] = random(2**16-1);
+      arg[1] = random(2**16-1);
+      res[0] = $signed(arg[0]) * $signed(arg[1]);
+      fork
+        for (int i = 0; i < 2; i++) arg_xmt(arg[i], i);
+        res_rcv(res[1]);
+      join
+      `test_equal(res[0], res[1]);
+    end
+  endtask
+
+  task testcase2;
+    logic [1:0][ARGW-1:0] arg;
+    logic [1:0][RESW-1:0] res;
+    repeat (8) begin
+      arg[0] = random(2**16-1);
+      arg[1] = random(2**16-1);
+      res[0] = $signed(arg[0]) * $signed(arg[1]);
+      fork
+        arg_xmt(arg[0], 0);
+        arg_xmt(arg[1], 1);
+        res_rcv(res[1]);
+      join
+      `test_equal(res[0], res[1]);
     end
   endtask
 
@@ -42,7 +73,9 @@ module testbench;
         $stop;
       end : timeout
       begin : worker
-        testcase;
+        testcase0;
+        testcase1;
+        testcase2;
         disable timeout;
       end : worker
     join
--- syn/src/multiply.v
+++ syn/src/multiply.v
@@ -4,51 +4,57 @@ module multiply #(
   input clk,
   input rst,
 
-  input arg_stb,
+  input [1:0] arg_stb,
   input [2*ARGW-1:0] arg_dat,
-  output arg_rdy,
+  output [1:0] arg_rdy,
 
   output reg res_stb,
   output reg [2*ARGW-1:0] res_dat,
   input res_rdy
 );
-  wire arg_ack = arg_stb & arg_rdy;
+  wire [1:0] arg_ack = arg_stb & arg_rdy;
   wire res_ack = res_stb & res_rdy;
   wire res_bsy = res_stb & ~res_rdy;
 
+  reg [1:0] int_stb = 2'b00;
   reg signed [ARGW-1:0] arg [1:0];
-  reg mul_stb = 0;
 
-  assign arg_rdy = ~mul_stb | res_rdy;
+  assign arg_rdy = ~int_stb | {2{&int_stb & res_rdy}};
 
+  integer i;
   always @(posedge clk) begin
-    if (arg_ack) begin
-      arg[0] <= arg_dat[0+:ARGW];
-      arg[1] <= arg_dat[ARGW+:ARGW];
+    for (i = 0; i < 2; i = i + 1) begin
+      if (arg_ack[i]) begin
+        arg[i] <= arg_dat[ARGW*i+:ARGW];
+      end
+    end
+  end
+
+  integer j;
+  always @(posedge clk) begin
+    for (j = 0; j < 2; j = j + 1) begin
+      if (rst) begin
+        int_stb[j] <= 0;
+      end else if (~int_stb[j] & arg_ack[j]) begin
+        int_stb[j] <= 1;
+      end else if (&int_stb & ~res_bsy) begin
+        int_stb[j] <= 0;
+      end
     end
   end
 
   initial res_stb = 0;
+
   always @(posedge clk) begin
     if (rst) begin
       res_stb <= 0;
-    end else if (mul_stb) begin
+    end else if (&int_stb) begin
       if (~res_stb) begin
         res_stb <= 1;
         res_dat <= arg[0] * arg[1];
       end
     end else if (res_ack) begin
       res_stb <= 0;
-    end
-  end
-
-  always @(posedge clk) begin
-    if (rst) begin
-      mul_stb <= 0;
-    end else if (~mul_stb & arg_ack) begin
-      mul_stb <= 1;
-    end else if (mul_stb & ~res_bsy) begin
-      mul_stb <= 0;
     end
   end
 
