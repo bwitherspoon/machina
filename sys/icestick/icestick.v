034190a9157445ee1e5d1f949d69daa183426dd4
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-19 13:30:41 -0600

    sys: icestick: add multiplier to top

--- sys/icestick/icestick.v
+++ sys/icestick/icestick.v
@@ -22,32 +22,94 @@ module icestick (
 );
   localparam FREQ = 12000000;
   localparam BAUD = 9600;
+  localparam BITS = 8;
 
-  wire err;
-  wire stb;
-  wire rdy;
-  wire [7:0] dat;
+  wire rcv_err;
+  wire rcv_stb;
+  wire rcv_rdy;
+  wire [7:0] rcv_dat;
+
+  wire sel_stb;
+  wire sel_rdy;
+  wire sel_dat;
+
+  wire [1:0] dux_stb;
+  wire [15:0] dux_dat;
+
+  wire [1:0] mul_rdy;
+  wire mul_stb;
+  wire [15:0] mul_dat;
+
+  wire upk_stb;
+  wire upk_rdy;
+  wire [7:0] upk_dat;
+
+  wire xmt_rdy;
 
   receive #(BAUD, FREQ) rcv (
     .clk(clk),
     .rst(1'b0),
     .rxd(rs232_rxd),
-    .rdy(rdy),
-    .stb(stb),
-    .dat(dat),
-    .err(err)
+    .rdy(rcv_rdy),
+    .stb(rcv_stb),
+    .dat(rcv_dat),
+    .err(rcv_err)
+  );
+
+  count #(2) sel (
+    .clk(clk),
+    .rst(1'b0),
+    .rdy(sel_rdy),
+    .stb(sel_stb),
+    .dat(sel_dat)
+  );
+
+  demultiplex #(.ARGW(8), .OUTC(2)) dux (
+    .clk(clk),
+    .rst(1'b0),
+    .arg_stb(rcv_stb),
+    .arg_dat(rcv_dat),
+    .arg_rdy(rcv_rdy),
+    .sel_stb(sel_stb),
+    .sel_dat(sel_dat),
+    .sel_rdy(sel_rdy),
+    .out_stb(dux_stb),
+    .out_dat(dux_dat),
+    .out_rdy(mul_rdy)
+  );
+
+  multiply #(.ARGW(8)) mul (
+    .clk(clk),
+    .rst(1'b0),
+    .arg_stb(dux_stb),
+    .arg_dat(dux_dat),
+    .arg_rdy(mul_rdy),
+    .res_stb(mul_stb),
+    .res_dat(mul_dat),
+    .res_rdy(upk_rdy)
+  );
+
+  unpack #(.ARGW(8), .ARGD(2)) upk (
+    .clk(clk),
+    .rst(1'b0),
+    .arg_stb(mul_stb),
+    .arg_dat(mul_dat),
+    .arg_rdy(upk_rdy),
+    .out_stb(upk_stb),
+    .out_dat(upk_dat),
+    .out_rdy(xmt_rdy)
   );
 
   transmit #(BAUD, FREQ) xmt (
     .clk(clk),
     .rst(1'b0),
-    .stb(stb),
-    .dat(dat),
-    .rdy(rdy),
+    .stb(upk_stb),
+    .dat(upk_dat),
+    .rdy(xmt_rdy),
     .txd(rs232_txd)
   );
 
-  assign led = {~err, {4{err}}};
+  assign led = {~rcv_err, {4{rcv_err}}};
 
   assign rs232_cts = 0;
   assign rs232_dcd = 0;
