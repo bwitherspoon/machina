541a048e22f0609d41e02a4861b23f6117d3ffc4
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-19 22:38:34 -0600

    inc: serial: force real values in delays

--- inc/serial.svh
+++ inc/serial.svh
@@ -7,23 +7,23 @@
   task prefix``stx(input [7:0] d); \
   begin \
     prefix``rxd = 0; \
-    #(1/(baud)/(unit)); \
+    #(1.0/(1.0*(baud))/(1.0*(unit))); \
     for (int i = 0; i < 8; i++) begin \
       prefix``rxd = d[i]; \
-      #(1/(baud)/(unit)); \
+      #(1.0/(1.0*(baud))/(1.0*(unit))); \
       end \
     prefix``rxd = 1; \
-    #(1/(baud)/(unit)); \
+    #(1.0/(1.0*(baud))/(1.0*(unit))); \
   end \
   endtask : prefix``stx \
   task prefix``srx(output [7:0] d); \
   begin \
-    wait (prefix``txd == 0) #(1.5/(baud)/(unit)); \
+    wait (prefix``txd == 0) #(1.5/(1.0*(baud))/(1.0*(unit))); \
     for (int i = 0; i < 8; i++) begin \
       d[i] = prefix``txd; \
-      #(1/(baud)/(unit)); \
+      #(1.0/(1.0*(baud))/(1.0*(unit))); \
     end \
-    #(1/(baud)/(unit)); \
+    #(1.0/(1.0*(baud))/(1.0*(unit))); \
   end \
   endtask : prefix``srx
 
