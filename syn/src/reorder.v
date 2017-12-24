3dfeef5d070430e91b815915003518c540cefd3b
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-24 16:53:07 -0600

    syn: reorder: fix output index bug

--- syn/src/reorder.v
+++ syn/src/reorder.v
@@ -24,10 +24,12 @@ module reorder #(
 
   wire adr_stb = mask == 0;
   wire adr_rdy;
-  reg [$clog2(N)-1:0] adr_dat = 0;
   wire [W-1:0] dat_dat;
+  reg [$clog2(N)-1:0] adr_dat[2];
 
   reg [N-1:0] mask = {N{1'b1}};
+
+  initial adr_dat[0] = 0;
 
   multiplex #(W, N) mux (
     .s_stb(s_stb & mask),
@@ -42,7 +44,7 @@ module reorder #(
     if (rst) begin
       mask <= {N{1'b1}};
     end else if (mask == 0) begin
-      if (adr_dat == END)
+      if (adr_dat[0] == END)
         mask <= {N{1'b1}};
     end else if (mux_stb & mux_rdy) begin
       mask <= mask & ~s_rdy;
@@ -59,7 +61,7 @@ module reorder #(
     .wdata_dat(mux_dat),
     .wdata_rdy(mux_dat_rdy),
     .raddr_stb(adr_stb),
-    .raddr_dat(adr_dat),
+    .raddr_dat(adr_dat[0]),
     .raddr_rdy(adr_rdy),
     .rdata_rdy(m_rdy),
     .rdata_stb(m_stb),
@@ -68,15 +70,16 @@ module reorder #(
 
   always @(posedge clk) begin
     if (rst) begin
-      adr_dat <= 0;
+      adr_dat[0] <= 0;
     end else if (adr_stb & adr_rdy) begin
-      if (adr_dat == END)
-        adr_dat <= 0;
+      adr_dat[1] <= adr_dat[0];
+      if (adr_dat[0] == END)
+        adr_dat[0] <= 0;
       else
-        adr_dat <= adr_dat + 1;
+        adr_dat[0] <= adr_dat[0] + 1;
     end
   end
 
-  assign m_dat = {adr_dat, dat_dat};
+  assign m_dat = {adr_dat[1], dat_dat};
 
 endmodule
