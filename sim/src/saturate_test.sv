41833b2e8338a3baf947b0d00beec596cae2ba96
Brett Witherspoon <spoonb@cdspooner.com>
2017-12-26 20:08:27 -0600

    sim: syn: saturate: rework

--- sim/src/saturate_test.sv
+++ sim/src/saturate_test.sv
@@ -1,5 +1,6 @@
 `include "check.svh"
 `include "dump.svh"
+`include "random.svh"
 
 module testbench;
   timeunit 1ns;
@@ -8,26 +9,26 @@ module testbench;
   localparam W = 16;
   localparam L = 8;
 
-  reg  [W-1:0] i;
-  wire [W-1:0] o;
+  localparam int MAX = 2**(L-1)-1;
+  localparam int MIN = -(2**(L-1));
+
+  logic signed [W-1:0] i;
+  logic signed [W-1:0] o;
 
   saturate #(W, L) uut (.*);
 
   task test;
-    logic [16:0] dat [4];
-    logic [15:0] sat [4];
-    begin
-      dat[0] = 16'h000f; dat[1] = 16'h00f0; dat[2] = 16'h007f; dat[3] = 16'h0100;
-      sat[0] = 16'h000f; sat[1] = 16'h00f0; sat[2] = 16'h007f; sat[3] = 16'h0080;
-      for (int n = 0; n < 4; n++) begin
-        i = dat[n];
-        #1 `check_equal(o, sat[n]);
-      end
+    int s;
+    repeat (25) begin
+      i = random(2**L);
+      s = MIN < i ? i < MAX ? i : MAX : MIN;
+      #1 `check_equal(o, s[W-1:0]);
     end
   endtask
 
   initial begin
     dump;
+    seed;
     test;
     $finish;
   end
--- syn/src/saturate.v
+++ syn/src/saturate.v
@@ -1,18 +1,11 @@
 module saturate #(
-  parameter [31:0] WIDTH = 16,
-  parameter [31:0] LIMIT = WIDTH - 1
+  parameter [31:0] W = 16,
+  parameter [31:0] S = W - 1
 )(
-  input  signed [WIDTH-1:0] i,
-  output signed [WIDTH-1:0] o
+  input signed [W-1:0] i,
+  output signed [W-1:0] o
 );
-  localparam signed [WIDTH-1:0] MAX = {{(WIDTH-LIMIT+1){1'b0}}, {(LIMIT-1){1'b1}}};
-  localparam signed [WIDTH-1:0] MIN = ~MAX;
-  reg signed [WIDTH-1:0] o;
-  always @*
-    case ({MIN < i, i < MAX})
-      2'b11: o = i[WIDTH-1:0];
-      2'b10: o = MAX;
-      2'b01: o = MIN;
-      2'b00: o = {WIDTH{1'bx}};
-    endcase
+  localparam signed [W-1:0] MAX = {{(W-S+1){1'b0}}, {(S-1){1'b1}}};
+  localparam signed [W-1:0] MIN = ~MAX;
+  assign o = MIN < i ? i < MAX ? i : MAX : MIN;
  endmodule
